9eb041f6932e41bd2345ede4b39c3bbe4cedcef1
Enxuan Zhou <aniken98@EnxuandeMacBook-Pro.local>
2019-05-09 04:17:19 -0700

    update code, map is showing

--- phaser/projectName/js/load.js
+++ phaser/projectName/js/load.js
@@ -2,11 +2,11 @@ var Load = function(game) {};
 Load.prototype = {
 	preload: function() {
         game.load.path = './assets/img/';
-        game.load.tilemap('level', 'map_temp.json', null, Phaser.Tilemap.TILED_JSON);
-        game.load.spritesheet('floor', 'sucai.png', 128, 128);
-		game.load.spritesheet('lava', 'danger.png', 128, 128);
-		game.load.spritesheet('enemy','baddie.png',32,32);
-		game.load.spritesheet('dude', 'dude.png',32,48);
+        game.load.tilemap('level', 'newnewnew.json', null, Phaser.Tilemap.TILED_JSON);
+        game.load.spritesheet('floor', 'sucai.png', 32, 32);
+	game.load.spritesheet('lava', 'danger.png', 32, 32);
+	game.load.spritesheet('enemy','baddie.png',32,32);
+	game.load.spritesheet('dude', 'dude.png',32,48);
 	},
 	create: function() {
 		
